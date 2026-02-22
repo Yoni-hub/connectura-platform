5bccb7849f57d328ddeddc1ed29916b09920629e
yonathan <yoniyona033@gmail.com>
2026-02-22 16:37:38 -0500

    Route staging /api paths to backend in nginx

--- automation/ssh/provision.sh
+++ automation/ssh/provision.sh
@@ -62,6 +62,18 @@ server {
     server_name ${DOMAIN};
     client_max_body_size 10m;
 
+    location ^~ /api/ {
+        rewrite ^/api/?(.*)$ /\$1 break;
+        proxy_pass http://127.0.0.1:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+
     location / {
         proxy_pass http://127.0.0.1:4173;
         proxy_set_header Host \$host;
